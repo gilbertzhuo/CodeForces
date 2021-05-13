4c16c13071818bbbfbeb812a465d9fa7a85a3109
gilbertzhuo <gilbertzhuo@gmail.com>
2021-05-13 12:06:08 +0800

    solved

---
+++ 339A/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+    string s;
+    char a[100];
+    cin >> s;
+    int j = 0;
+    for (int i = 0; i < s.length(); ++i) {
+        if (s[i] != '+') {
+            a[j++] = s[i];
+        }
+    }
+    sort(a, a+j);
+    for(int i = 0; i < j; ++i) {
+        if (i == j-1) cout<<a[i]<<endl;
+        else cout<<a[i]<<"+";
+    }
+    
+    return 0;
+}
